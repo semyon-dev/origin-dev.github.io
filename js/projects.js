3983d7da32a95d721d0657e64b36e9b48d7167f0
Ilya Iskra <ilya.iskra2020@gmail.com>
2021-10-16 21:52:36 +0300

    Projects card rendering

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,8 @@
 // variables
-const scrollToTopButton = document.querySelector('.scroll-to-top');
+const projectsContent = document.querySelector('.projects__content');
+const projectTemplate = document.querySelector('#projects__template').content;
 
+const scrollToTopButton = document.querySelector('.scroll-to-top');
 
 //code
 let scrollToTopAnimation = new SmoothScroll('.scroll-to-top', {
@@ -9,9 +11,31 @@ let scrollToTopAnimation = new SmoothScroll('.scroll-to-top', {
   easing: 'easeInOutCubic',
   topOnEmptyHash: true
 });
+fetch("../content/content.json")
+  .then(response => response.json())
+  .then(json => {
+    // projects rendering
+    const projects = json.projects;
+    for (let i = 0; i < projects.length; i++) {
+      const project = projectTemplate.cloneNode(true);
+      const image = project.querySelector('.project-card__image');
+      image.children[0].srcset = `./img/${projects[i].imageName}.webp`;
+      image.children[1].srcset = `./img/${projects[i].imageName}.jpg`;
+      image.children[2].alt = `${projects[i].title} logo`;
+      project.querySelector('.project-card__title').textContent = projects[i].title;
+      project.querySelector('.project-card__description').textContent = projects[i].description;
+      project.querySelector('.project-card__button').href = `./projects.html#${i}`;
+      projectsContent.append(project);
+    }
+  });
+
 
 
 // event listeners
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.classList.remove('preload-transition');
+});
+
 document.addEventListener('scroll', e => {
   //scroll-to-top button
   if (window.scrollY >= window.innerHeight) {
