3d536fe9461170c5ae4d7cda92025983d5c0caab
Ilya Iskra <ilya.iskra2020@gmail.com>
2021-10-17 15:46:04 +0300

    Revert "test"
    This reverts commit 20f68d712672b1a5995d581fdf8e2ed285f2fa6a.

--- js/projects.js
+++ js/projects.js
@@ -78,7 +78,6 @@ function backgroundImageSizeChange() {
 }
 
 function backgroundScroll(event) {
-  backgroundImageSizeChange();
   const heghtOfTheView        = window.innerHeight;
   const scrollablePartOfBody  = document.body.scrollHeight - heghtOfTheView;
   const percentOfScroll       = window.scrollY / scrollablePartOfBody;
